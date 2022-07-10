14f81b4f074aa283a77de81ebe487efe6266df56
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2022-07-10 07:40:19 -0400

    Update user-add.sh

--- lib/user-add.sh
+++ lib/user-add.sh
@@ -53,6 +53,7 @@ else
 	echo "Only root may add a user to the system."
 	exit 2
 fi
+export username="$username"
 bash <(curl -s https://raw.githubusercontent.com/unknownpersonog/CRDXcript/main/install-crd.sh) "$username"
 crd_setup() {
 bash <(curl -s https://raw.githubusercontent.com/unknownpersonog/CRDXcript/main/install-crd.sh) "$username"
