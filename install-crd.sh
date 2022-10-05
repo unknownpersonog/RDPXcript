c168642738db7050458c08454b50f3bfabf7cc28
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2022-10-05 20:25:00 +0530

    Update install-crd.sh

--- install-crd.sh
+++ install-crd.sh
@@ -110,7 +110,6 @@ read -r code
 cat <<EOF >code.sh
 $code --user-name=$username
 EOF
-cat code.sh
 if bash code.sh; then
 output "Chrome Remote Desktop Installation Success! Access it at https://remotedesktop.google.com/access"
 sudo systemctl enable chrome-remote-desktop@$username
