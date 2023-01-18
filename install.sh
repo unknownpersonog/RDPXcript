81f6faa4a19339dbcedf8d8d1172311ecf839679
dxomg <101947415+dxomg@users.noreply.github.com>
2023-01-18 15:51:30 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 set -e
 
 
-SCRIPT_VERSION="main"
-GITHUB_BASE_URL="https://raw.githubusercontent.com/dxomg/rdpscript/"
+SCRIPT_VERSION="v2.1"
+GITHUB_BASE_URL="https://raw.githubusercontent.com/unknownpersonog/RDPXcript"
 
 LOG_PATH="/var/log/RDPXcript.log"
 # exit with error status code if user is not root
